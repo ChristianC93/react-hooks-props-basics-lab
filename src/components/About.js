2217d23bcaed3058821ea9a9c8232ca3b5750c21
Christian Castillo <castillochristian3@gmail.com>
2022-05-03 14:51:31 -0400

    commit tree 374d30a1805612d73eaffd20f9a6b280af2b948d

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,27 @@
 import React from "react";
+import user from "../data/user";
 
-function About() {
+function About(props) {
+  if (!props.bio) {
+    return null
+  } else {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links links={user.links} github={"https://github.com/liza"} linkedin={"https://www.linkedin.com/in/liza/"}/>
+    </div>
+  );}
+}
+
+function Links(props) {
+  //console.log(props)
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
     </div>
   );
 }
